525fb56a9128b3d32fd98bb0e95a6b516ecb279b
Tyler Goldsmith <tyler.r.goldsmith@gmail.com>
2021-11-17 17:29:49 -0500

    Finished html/js code

--- public/index.js
+++ public/index.js
@@ -1,28 +1,40 @@
 async function main() {
 
     let response = await fetch('http://localhost:3001/listBooks')
+
     let books = await response.json()
 
     books.forEach(renderBook)
 }
 
 function renderBook(book) {
-    let bookContainer = document.querySelector('.book-container')
-    bookContainer.innerHTML += `
-        <div class="col-sm-3">
-            <div class="card" style="width: 100%;">
-                ${book.imageURL ? `
-                    <img class="card-img-top" src="${book.imageURL}" />
-                `
-                : ``}
-                <div class="card-body">
-                    <h5 class="card-title">${book.title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Available: ${book.quantity}</h6>
-                    <p class="card-text">${book.description}</p>
-                </div>
-            </div>
-        </div>
-    `
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+
+    li.append(quantityInput, saveButton)
+
+    root.append(li)
 }
 
-main()
+main();
