e3e15f3ad9cd06e9d844134fa63ec49f30e9887c
Tyler Goldsmith <tyler.r.goldsmith@gmail.com>
2021-11-18 00:43:38 -0500

    Cleaning up code

--- public/index.js
+++ public/index.js
@@ -1,29 +1,28 @@
-async function main() {
+const book_url ="http://localhost:3001/listBooks"
+const update_url ="http://localhost:3001/updateBooks"
+const root = document.querySelector("#root")
 
-    let response = await fetch('http://localhost:3001/listBooks')
-
-    let books = await response.json()
-
-    books.forEach(renderBook)
+async function getBooks() {
+    let response = await fetch(books_url)
+    let data = await response.json()
+    data.forEach(renderBook)
 }
 
 function renderBook(book) {
-    let root = document.querySelector('#root')
+    let li = document.createElement("li")
+        li.textContent = book.title
+    let inputQuantity = document.createElement("input")
+        quantityInput.value = book.quantity
+    let saveButton = document.createElement("button")
+        saveButton.textContent = "Save"
 
-    let li = document.createElement('li')
-    li.textContent = book.title
-
-    let quantityInput = document.createElement('input')
-    quantityInput.value = book.quantity
-
-    let saveButton = document.createElement('button')
-    saveButton.textContent = 'Save'
+    root.append(li, input, saveButton)
 
     saveButton.addEventListener('click', () => {
-        fetch('http://localhost:3001/updateBook', {
-            method: 'PATCH',
+        fetch(update_url, {
+            method: "PATCH",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json"
             },
             body: JSON.stringify({
                 id: book.id,
@@ -32,9 +31,6 @@ function renderBook(book) {
         })
     })
 
-    li.append(quantityInput, saveButton)
-
-    root.append(li)
 }
 
-main();
+getBooks();
